3aa5767340f24d18507d44c93f4a98676b45d77d
Olego0 <ostrovskij.013g@gmail.com>
2020-06-24 13:12:15 +0300

    test

--- test/test.js
+++ test/test.js
@@ -30,12 +30,28 @@ let lname=document.getElementById('surnames');
 let time=document.getElementById('times');
 
 document.getElementById('send').addEventListener('click', sent);
-
+let ggg;
 function sent(){
-     let ggg = {
+    ggg = {
         name: fname.value,
         surname: lname.value,
         date: time.value
     }
     console.log(ggg);
 }
+// ================================================================
+document.getElementsByTagName('a')[0].onclick = function() {                  
+    let csv = `Name: ${ggg.name} ${ggg.surname}`;
+    let csvs = `Date: ${ggg.date}`;
+    let csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv) + ';' + encodeURIComponent(csvs);
+    this.href = csvData;
+    // this.target = '_blank';
+    this.download = 'filename.txt';
+    // fs.writeFileSync("filename.txt", `Name: ${ggg.name} ${ggg.surname} Date: ${ggg.date}`);
+    // let text = `Name: ${ggg.name} ${ggg.surname} Date: ${ggg.date}`
+    // document.write('<a href="text.txt;charset=utf-8,%EF%BB%BF' + encodeURIComponent(text) + '">text.txt</a>');
+    
+};
+// ================================================================
+
+
