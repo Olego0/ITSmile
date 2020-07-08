b7cec1c2a2e3e2a2d05a7d681e2ee4832344d9fa
Olego0 <48787867+Olego0@users.noreply.github.com>
2020-07-08 13:17:39 +0300

    Add files via upload

--- test/test.js
+++ test/test.js
@@ -1,60 +1,47 @@
-let fname=document.getElementById('names');
-let lname=document.getElementById('surnames');
-let time=document.getElementById('times');
+let divblock = [
+    ("div1"),
+    ("div2"),
+    ("div3"),
+    ("div4"),
+    ("div5"),
+    ("div6"),
+    ("div7"),
+    ("div8"),
+    ("div9"),
+    ("div10"),
+    ("div11"),
+    ("div12")
+]
 
-document.getElementById('send').addEventListener('click', sent);
-let ggg;
-let password;
-let passwordOne;
-function sent(){
-    ggg = {
-        name: fname.value,
-        surname: lname.value,
-        date: time.value
-    }
-    console.log(ggg);
-}
-// ================================================================
-document.getElementsByTagName('a')[0].onclick = function() {  
-    console.log(variants.value);             
-    if(variants.value == 1){
-        password = "-пришел";
-        passwordOne = "    пришел/ушел: пришел";
-    } else{
-        password = "-ушел";
-        passwordOne = "    пришел/ушел: ушел";
-    }
-    let csv = `Name: ${ggg.name} ${ggg.surname}`;
-    let csvs = `    Date: ${ggg.date}`;
-    let csvData = 'data:application/csv;charset=utf-8,' + 
-                    encodeURIComponent(csv) + ';' + 
-                    encodeURIComponent(csvs) + ';' + 
-                    encodeURIComponent(passwordOne) + ';';
-    this.href = csvData;
-    this.download = ggg.name + ' ' + 
-                    ggg.surname + 
-                    password + '.txt';
-    window.location.reload()
-};
-const variants = document.getElementById("variants");
-// ====================DarkMode====================
-const round = document.querySelector('.round');
-const body = document.querySelector('body');
-let mode = false;
-round.addEventListener('click', eventHandler);
-function eventHandler({ target }) {
-    if (mode === false) {
-        mode = true;
-        body.style.background = 'aqua';
-        body.style.transition = 1 + "s";
-        round.style.background = 'cornsilk';
-        round.style.transition = 1 + "s";
+let random = Math.floor(Math.random()*12);
+console.log(random);
 
-    } else if (mode === true) {
-        mode = false;
-        body.style.background = '#3e4243';
-        body.style.transition = 1 + "s";
-        round.style.background = '#608ae4';
+
+
+let conf = confirm("Точно ли запустить?");
+/* if (conf==true){
+    document.getElementById(divblock[random]).style.backgroundColor="red";
+} */
+
+for (let i =0; i<999; i++){
+    /* conf = confirm("Точно ли запустить?")
+    if (conf==true){
+        document.getElementById(divblock[random]).style.backgroundColor="red";
     }
+    if (conf==false){
+        break;
+    } */
+
+
+    function switchcolor () {
+        document.getElementById(divblock[random]).style.backgroundColor="red";
+    }
+    
+    function User (callback){
+        let conf = confirm("Точно ли запустить?");
+        callback();
+    }
+    
+    User(switchcolor);
 }
-// ================================================
+
