a9a1979170455ffec4eeb1ce033eab78549ffe46
Oleh <48787867+Olego0@users.noreply.github.com>
2020-07-17 15:02:56 +0300

    Add files via upload

--- test/test.js
+++ test/test.js
@@ -1,47 +1,49 @@
-let divblock = [
-    ("div1"),
-    ("div2"),
-    ("div3"),
-    ("div4"),
-    ("div5"),
-    ("div6"),
-    ("div7"),
-    ("div8"),
-    ("div9"),
-    ("div10"),
-    ("div11"),
-    ("div12")
-]
+const db = {
+    1111:{},
+    2222:{},
+    3333:{},
+    4444:{},
+    5555:{},
+    6666:{},
+    7777:{}
+};
 
-let random = Math.floor(Math.random()*12);
-console.log(random);
-
-
-
-let conf = confirm("Точно ли запустить?");
-/* if (conf==true){
-    document.getElementById(divblock[random]).style.backgroundColor="red";
-} */
-
-for (let i =0; i<999; i++){
-    /* conf = confirm("Точно ли запустить?")
-    if (conf==true){
-        document.getElementById(divblock[random]).style.backgroundColor="red";
-    }
-    if (conf==false){
-        break;
-    } */
-
-
-    function switchcolor () {
-        document.getElementById(divblock[random]).style.backgroundColor="red";
-    }
+function play(){
     
-    function User (callback){
-        let conf = confirm("Точно ли запустить?");
-        callback();
+    let pass="";
+    let number="";
+    let bet="";
+
+    while (pass!=null){
+        pass=prompt("Write your pass")
+        for (let key in db){
+            if (pass==key){
+                while(true){
+                    number=prompt("Your number")
+                    if (number==null){
+                        break;
+                    }
+
+                    bet=prompt("Your bet in number")
+
+                    db[key][number]=bet
+                }
+                break;
+                }
+            }
+        }
+        let a =Math.floor(Math.random()*11)
+        for(let klue in db){
+            if(typeof(db[klue])=='object'){
+                for (let d in db[klue]){
+                    if(a==d)
+                    {
+                        console.log(`player ${klue} win ${db[klue][d]*10}`);
+                    }
+                    else {
+                        console.log(`player ${klue} lose. Number is ${a}`);
+                    }
+            }
+        }
     }
-    
-    User(switchcolor);
 }
-
