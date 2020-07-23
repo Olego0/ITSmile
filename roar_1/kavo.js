029ecf0a4cf1b65c3a4190dcdd24ca52f480970d
Oleh <48787867+Olego0@users.noreply.github.com>
2020-07-23 13:11:11 +0300

    Add files via upload

---
+++ roar_1/kavo.js
@@ -0,0 +1,331 @@
+const db = {
+    1:{},
+    2:{},
+    3:{},
+    4:{}
+}
+
+let points=0;
+let points1=0;
+
+let a;
+let b;
+let c;
+let ok4=true;
+let ok2=true;
+let ok3=true;
+
+
+function mathQuizEasy() {
+    let pass="";
+    let q="";
+    let x="My points";
+    while (pass!=null) {
+        pass= prompt('Password?');
+        for (let key2 in db) {
+            if (pass==key2) {
+                while(ok4==true){
+                    q=Math.floor(Math.random()*3);
+                    w="";
+                    if(q==0){
+                        a=Math.floor(2+Math.random()*7);
+                        b=Math.floor(2+Math.random()*7);
+                        c=a*b;
+                        w=prompt(a+"*"+b+'=?');
+                        if (w==c&&w!=null) {
+                            confirm("You won 1 point!");
+                            points1++;
+                        }else if(w!=null){
+                            confirm("You lost 1 point!");
+                            points1--;
+                        }
+                        
+                    }
+
+                    if(q==1){
+                        a=Math.floor(2+Math.random()*19);
+                        b=Math.floor(2+Math.random()*19);
+                        c=a+b;
+                        w=prompt(a+"+"+b+'=?');
+                        if (w==c&&w!=null) {
+                            confirm("You won 1 point!");
+                            points1++;
+                            
+                            
+                        }else if(w!=null){
+                            confirm("You lost 1 point!");
+                            points1--;
+                        }
+                        
+                    }
+
+                    if(q==2){
+                        a=Math.floor(2+Math.random()*19);
+                        b=Math.floor(2+Math.random()*19);
+                        if(a<b){
+                            c=b-a;
+                            w=prompt(b+"-"+a+'=?');
+                        }else{
+                            c=a-b;
+                            w=prompt(a+"-"+b+'=?');
+                        }
+                        if (w==c&&w!=null) {
+                            confirm("You won 1 point!");
+                            points1++;
+                        }else if(w!=null){
+                            confirm("You lost 1 point!");
+                            points1--;
+                        }
+                        
+                    }
+                    ok4=confirm("One more?");
+                    db[key2][x]=points1;
+                    console.log(db);
+                    document.getElementById("1").innerHTML="Yor ballance is "+points1+' points';
+                }
+                    
+                
+                break;
+            }
+            
+        }
+        
+    }
+}
+
+    
+
+
+function mathQuizNormal() {
+    let pass="";
+    let q="";
+    let x="My points";
+    while (pass!=null) {
+        pass= prompt('Password?');
+        for (let key3 in db) {
+            if (pass==key3) {
+                while (ok2==true) {
+                    q=Math.floor(Math.random()*3);
+                    w="";
+                    if(q==0){
+                        a=Math.floor(10+Math.random()*10);
+                        b=Math.floor(10+Math.random()*10);
+                        c=a*b;
+                        w=prompt(a+"*"+b+'=?');
+                        if (w==c&&w!=null) {
+                            confirm("You won 2 points!");
+                            points1+=2;
+                        }else if(w!=null){
+                            confirm("You lost 2 points!");
+                            points1-=2;
+                        }
+                        
+                    }
+
+                    if(q==1){
+                        a=Math.floor(10+Math.random()*91);
+                        b=Math.floor(10+Math.random()*91);
+                        c=a+b;
+                        w=prompt(a+"+"+b+'=?');
+                        if (w==c&&w!=null) {
+                            confirm("You won 2 points!");
+                            points1+=2
+                        }else if(w!=null){
+                            confirm("You lost 2 points!");
+                            points1-=2
+                        }
+                        
+                    }
+
+                    if(q==2){
+                        a=Math.floor(10+Math.random()*91);
+                        b=Math.floor(10+Math.random()*91);
+                        if(a<b){
+                            c=b-a;
+                            w=prompt(b+"-"+a+'=?');
+                        }else{
+                            c=a-b;
+                            w=prompt(a+"-"+b+'=?');
+                        }
+                        if (w==c&&w!=null) {
+                            confirm("You won 2 point!");
+                            points1+=2
+                        }else if(w!=null){
+                            confirm("You lost 2 point!");
+                            points1-=2
+                        }
+                        
+                    }
+                    ok2=confirm("One more?");
+                    db[key3][x]=points1;
+                    console.log(db);
+                    document.getElementById("1").innerHTML="Yor ballance is "+points1+' points';
+                }
+                    
+                
+                break;
+            }
+            
+        }
+        
+    }
+        
+}
+    
+
+
+function mathQuizHard() {
+    let pass="";
+    let q="";
+    let x="My points";
+    while (pass!=null) {
+        pass= prompt('Password?');
+        for (let key4 in db) {
+            if (pass==key4) {
+                while(ok3==true){
+                    q=Math.floor(Math.random()*3);
+                    w="";
+                    if(q==0){
+                        a=Math.floor(1+Math.random()*100);
+                        b=Math.floor(1+Math.random()*100);
+                        c=a*b;
+                        w=prompt(a+"*"+b+'=?');
+                        if (w==c&&w!=null) {
+                            confirm("You won 5 points!");
+                            points1+=5;
+                        }else if(w!=null){
+                            confirm("You lost 5 points!");
+                            points1-=5;
+                        }
+                        
+                    }
+
+                    if(q==1){
+                        a=Math.floor(100+Math.random()*901);
+                        b=Math.floor(100+Math.random()*901);
+                        c=a+b;
+                        w=prompt(a+"+"+b+'=?');
+                        if (w==c&&w!=null) {
+                            confirm("You won 5 points!");
+                            points1+=5;
+                        }else if(w!=null){
+                            confirm("You lost 5 points!");
+                            points1-=5;
+                        }
+                        
+                    }
+
+                    if(q==2){
+                        a=Math.floor(100+Math.random()*901);
+                        b=Math.floor(100+Math.random()*901);
+                        if(a<b){
+                            c=b-a;
+                            w=prompt(b+"-"+a+'=?');
+                        }else{
+                            c=a-b;
+                            w=prompt(a+"-"+b+'=?');
+                        }
+                        if (w==c&&w!=null) {
+                            confirm("You won 5 points!");
+                            points1+=5;
+                        }else if(w!=null){
+                            confirm("You lost 5 points!");
+                            points1-=5;
+                        }
+                        
+                    }
+                    ok3=confirm("One more?");
+                    db[key4][x]=points1;
+                    console.log(db);
+                    document.getElementById("1").innerHTML="Yor ballance is "+points1+' points';
+                }
+                    
+                
+                break;
+            }
+            
+        }
+        
+    }
+        
+}
+
+function RandomLevel() {
+    let ggg=Math.floor(Math.random()*3);
+    if (ggg==0) {
+        mathQuizEasy();
+    }
+    if (ggg==1) {
+        mathQuizNormal();
+    }
+    if (ggg==2) {
+        mathQuizHard();
+    }   
+}
+    
+function AskLevel() {
+
+    let gg=prompt("What level do you want to play at?(easy - 0, normal - 1, hard - 2");
+    
+    if (gg==0) {
+        mathQuizEasy();
+    }
+    if (gg==1) {
+        mathQuizNormal();
+    }
+    if (gg==2) {
+        mathQuizHard();
+    }
+}
+
+function play() {
+    let pass="";
+    let number="";
+    let bet="";
+    let x="My points";
+
+    while (pass!=null) {
+        pass= prompt('Password?');
+        for (let key in db) {
+            if (pass==key) {
+                while(true){
+                    number=prompt("your number?");
+                    if (number==null){
+                        break;
+                    }
+
+                    bet=prompt("bet?");
+
+                    db[key][number]=bet;
+                    
+                }
+                break
+            }
+            
+        }
+        
+    }
+    let a=Math.floor(Math.random()*37);
+    for(key1 in db){
+        if(typeof(db[key1])=='object'){
+            for(let d in db[key1]){
+                if(a==d){
+                    console.log(`player ${key1} win ${db[key1][d]}*36 points`);
+                    let y=bet*36;
+                    points1+=y;
+                    db[key1][x]=points1;
+                    console.log(db);
+                    document.getElementById("1").innerHTML="Yor ballance is "+points1+' points';
+                    
+                }else{
+                    console.log(`player ${key1} lost. The number was ${a}`);
+                    points1-=bet;
+                    db[key1][x]=points1;
+                    console.log(db);
+                    document.getElementById("1").innerHTML="Yor ballance is "+points1+' points';
+                    
+                }
+            } 
+        }
+    }
+}
