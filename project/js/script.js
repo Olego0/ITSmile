a0cfe90c07617120e5adf8d6b2a2fec58da545bd
Oleh <48787867+Olego0@users.noreply.github.com>
2020-07-30 13:13:15 +0300

    Update script.js

--- project/js/script.js
+++ project/js/script.js
@@ -24,6 +24,40 @@ const movieDB = {
     ]
 };
 
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+let add = document.querySelector(".promo__adv");
+console.log(add);
+// add.remove();
+let addOne = add.querySelectorAll("img");
+console.log(addOne);
+addOne[0].remove();
+// addOne.pop();
+
+let name = document.querySelector(".promo__genre").textContent = "драма";
+
+let hhh=document.querySelector(".promo__bg").style.backgroundImage="url('img/bg.jpg')";
+
+// for (let key in movieDB.movies) {
+//     // console.log(movieDB.movies[key]);
+// }
+let list = document.querySelector(".promo__interactive-list");
+console.log(list);
+list.innerHTML =  "";
+movieDB.movies[5] = "ggggggggggg";
+movieDB.movies.forEach((e, i)  => {
+    list.innerHTML += `<li class="promo__interactive-item">${i + 1}:${e}<div class="delete"></div></li>`;
+});
+
 
 
 
